427f2b46e0b4ed66a69579fb7bafe90268b10adc
Kseniya Radiseva <Kotaviy@gmail.com>
2023-08-10 17:57:23 +0500

    TestNG

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashBackHackerTest {
+    @org.testng.annotations.Test
+    public void remain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void remainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+}
